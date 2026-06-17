efb45b399a507cf11f513ce677223735e797121a
Shogun_Sun <fima8516@gmail.com>
2026-06-17 19:35:09 +0300

    chore: add dedicated API container and reorganize docker structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,28 +3,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"academic-booking-api/internal"
 	"academic-booking-api/internal/client"
 )
 
-const (
-	ODataURL   = "http://localhost:8080/booking/odata/standard.odata/"
-	Username   = "administrator"
-	Password   = ""
-	ServerPort = ":4000"
-)
-
 func main() {
 	log.Println("Запуск модульной API-обертки...")
 
-	odataClient := client.NewODataClient(ODataURL, Username, Password)
+	odataURL := os.Getenv("ODATA_URL")
+	username := os.Getenv("ODATA_USERNAME")
+	password := os.Getenv("ODATA_PASSWORD")
+	serverPort := os.Getenv("GO_SERVER_PORT")
+
+	if odataURL == "" {
+		log.Fatal("Переменная окружения ODATA_URL не задана!")
+	}
+	if username == "" {
+		log.Fatal("Переменная ODATA_USERNAME не задана!")
+	}
+	if serverPort == "" {
+		serverPort = ":4000"
+	}
+
+	odataClient := client.NewODataClient(odataURL, username, password)
 	log.Printf("Клиент OData настроен на адрес: %s\n", odataClient.BaseURL)
 
 	router := internal.SetupRoutes(odataClient)
 
-	log.Printf("Go-сервер успешно запущен на http://localhost%s\n", ServerPort)
-	if err := http.ListenAndServe(ServerPort, router); err != nil {
+	log.Printf("Go-сервер запущен на порту %s\n", serverPort)
+	if err := http.ListenAndServe(":"+serverPort, router); err != nil {
 		log.Fatalf("Не удалось запустить сервер: %v", err)
 	}
 }
